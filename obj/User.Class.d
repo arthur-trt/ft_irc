2dc537ae7267b10e2ad0c3f083eb52339fdb33ad
Arthur-TRT <atrouill@student.42.fr>
2022-05-06 17:55:27 +0200

    Cleanning

--- obj/User.Class.d
+++
@@ -1,5 +0,0 @@
-obj/User.Class.o: srcs/User.Class.cpp includes/User.Class.hpp \
-  includes/utils.hpp
-srcs/User.Class.cpp:
-includes/User.Class.hpp:
-includes/utils.hpp:
--- obj/main.d
+++
@@ -1,3 +0,0 @@
-obj/main.o: srcs/main.cpp includes/User.Class.hpp
-srcs/main.cpp:
-includes/User.Class.hpp:
